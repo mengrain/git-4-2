9567c0784cf3bb8cba9a42fb44e4e968dcbe59d7
mengrain <l>
2018-04-02 21:02:52 +0800

    Signed-off-by: mengrain <l>

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+var mysql = require('mysql')
+var app = express()
+
+app.use(bodyparser.urlencoded({}))
+var pool = mysql.createPool({
+	host:'127.0.0.1',
+	user:'root',
+	password:'yu62417..',
+	database:'items',
+	port:3306
+}) 
+
+//查询
+app.get('/',(req,res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	pool.getConnection(function(err,connection){
+		if(err){
+			console.log(err)
+			return
+		}
+		var sql = 'select * from loupan'
+		connection.query(sql,function(err,data){
+			if(err){
+				console.log(err)
+				return
+		    }
+			res.send(data)
+			connection.end()
+		})
+	})
+})
+
+
+
+app.listen(3000,function(){
+	console.log('ok')
+})
